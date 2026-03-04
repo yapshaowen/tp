1062420fb016fe0697055a38548fd48bf378022f
Shao Wen <shaokey2004@gmail.com>
2026-03-04 22:43:48 +0800

    Add remark field and command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,38 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ */
+public class Remark {
+
+    public final String value;
+
+    /**
+     * Makes a remark
+     *
+     * @param remark
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof Remark
+                && value.equals(((Remark) other).value));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
